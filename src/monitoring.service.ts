e9520cc976379e590b79565a9c0c4c23cdc7c200
Alexey Tsymbal <tsymbal.aleksey@gmail.com>
2022-07-14 15:59:47 +0400

    Add sinks initialization

--- src/monitoring.service.ts
+++ src/monitoring.service.ts
@@ -7,7 +7,23 @@ export class MonitoringService implements OnModuleInit {
   constructor(private readonly sdk: DialectSdkDecorator) {}
 
   onModuleInit() {
-    Monitors.builder({ sdk: this.sdk });
+    Monitors.builder({
+      sdk: this.sdk,
+      sinks: {
+        sms: {
+          twilioUsername: process.env.TWILIO_ACCOUNT_SID!,
+          twilioPassword: process.env.TWILIO_AUTH_TOKEN!,
+          senderSmsNumber: process.env.TWILIO_SMS_SENDER!,
+        },
+        telegram: {
+          telegramBotToken: process.env.TELEGRAM_TOKEN!,
+        },
+        email: {
+          apiToken: process.env.SENDGRID_KEY!,
+          senderEmail: process.env.SENDGRID_EMAIL!,
+        },
+      },
+    });
     // Add monitoring service
   }
 }
